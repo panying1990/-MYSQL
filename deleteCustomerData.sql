2392b492e7844044e02a378c69c04eb76fe675c2
panying <panying1298247841@163.com>
2017-08-28 14:31:25 +0800

    Update deleteCustomerData.sql

--- deleteCustomerData.sql
+++ deleteCustomerData.sql
@@ -1,2 +1,6 @@
--- 用于删除钻展数据中符合要求的原始数据
-Delete table mkt_ztc_data WHERE dateformat(）
+-- 用于删除钻展数据中符合要求的原始数据,该流程一般不可逆，慎重使用
+DELETE
+FROM
+ mkt_tbk_data
+WHERE
+ DATE_FORMAT(确认收货时间,"%y-%m-%d") BETWEEN "17-08-13" AND "17-08-20";
